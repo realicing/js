6b7ba18587cd6a42d85d84038ab0848b917ef958
realicing <realicing@sina.com>
2019-12-04 01:18:18 +0800

    添加提示

--- realicingjx.js
+++ realicingjx.js
@@ -1,6 +1,26 @@
-
+// ==UserScript==
+// @name              VIP解析插件
+// @namespace         https://www.baidu.com
+// @version           1.0.0
+// @icon              https://www.baidu.com/favicon.ico
+// @description       VIP接口解析
+// @author            realicing
+// @license           MIT
+// @supportURL        https://www.baidu.com
+// @match             *.le.com/*
+// @match             *.iqiyi.com/*
+// @match             *.youku.com/*
+// @match             *.letv.com/*
+// @match             *v.qq.com/*
+// @match             *.tudou.com/*
+// @match             *.mgtv.com/*
+// @match             *.sohu.com/*
+// @run-at            document-idle
+// @grant             unsafeWindow
+// ==/UserScript==
 (function () {
-    const list = [
+    alert("start");
+    var list = [
         "https://api.lhh.la/vip/?url=",
         "http://www.82190555.com/index/qqvod.php?url=",
         "http://jiexi.071811.cc/jx2.php?url=",
@@ -13,8 +33,8 @@
         "https://www.playm3u8.cn/jiexi.php?url=",
         "http://vip.jlsprh.com/index.php?url=",
     ];
-    let list2 = [];
-    const URL_START_LIST = [
+    var list2 = [];
+    var URL_START_LIST = [
         "https://www.iqiyi.com/v_",
         "https://v.qq.com/x/cover/",
         "https://v.youku.com/v_show/",
@@ -24,16 +44,15 @@
         "https://tv.sohu.com/v/",
         // "",
     ];
-    let videoPanel;
-    let buttonsPanel;
-    let curUrl = window.location.toString();
-    let needInitVideo = false;
+    var videoPanel;
+    var buttonsPanel;
+    var curUrl = window.location.toString();
+    var needInitVideo = false;
     main();
     function main() {
         URL_START_LIST.forEach(item => {
             needInitVideo |= curUrl.startsWith(item);
         });
-
         if (needInitVideo) {
             initVideo(curUrl);
             initButtons(curUrl);
@@ -45,11 +64,10 @@
             });
         }
     }
-
     //监听url变化
     function listenUrlChange(callBack) {
         setTimeout(() => {
-            let newUrl = window.location.toString();
+            var newUrl = window.location.toString();
             if (curUrl !== newUrl) {
                 callBack && callBack(curUrl, newUrl);
                 curUrl = newUrl;
@@ -57,11 +75,8 @@
             listenUrlChange(callBack);
         }, 500);
     }
-
-
-    //生成右下角dom按钮
     function initButtons(srcLink) {
-        if (buttonsPanel == null){
+        if (buttonsPanel == null) {
             buttonsPanel = document.createElement('div');
             buttonsPanel.setAttribute("style", `
                 position: fixed;
@@ -74,15 +89,15 @@
                 border-radius: 3px;
                 color: white;`);
         }
-        let html = `<div class="btn-area">`;
+        var html = `<div class="btn-area">`;
         list.forEach(item => {
-            let link = item + srcLink;
+            var link = item + srcLink;
             html += `<a href="` + link + `">
                         <button>` + item + `</button>
                      </a></br>`
         });
         list2.forEach(({name, value}) => {
-            let link = value + srcLink;
+            var link = value + srcLink;
             html += `<a href="` + link + `">
                         <button>` + name + `</button>
                      </a></br>`
@@ -91,7 +106,6 @@
         buttonsPanel.innerHTML = html;
         document.body.appendChild(buttonsPanel);
     }
-
     //播放
     function initVideo(link) {
         if (videoPanel == null) {
@@ -110,10 +124,10 @@
                 color: white;`);
         }
         document.body.appendChild(videoPanel);
-        let html = `<iframe position="sticky" top="30px" right="5px" width="100%" height="100%" src="https://api.tv920.com/vip/?url=` + link +
+        var html = `<iframe position="sticky" top="30px" right="5px" width="100%" height="100%" src="https://api.tv920.com/vip/?url=` + link +
             `" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen"  mozallowfullscreen="mozallowfullscreen"  msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" ></iframe>
 	`;
         videoPanel.innerHTML = html;
     }
-
+    alert("end");
 })();
